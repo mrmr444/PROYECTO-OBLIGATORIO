43c17fb221e8c853f8f263d1b5fd06d1cdac39bd
Marcos Ramos <marcosr_444@hotmail.com>
2020-10-25 14:27:02 -0300

    intento de validacion de formulario
    No sale

--- js/cart.js
+++ js/cart.js
@@ -20,10 +20,11 @@ function calcSubtotal(precioUnidad, i) {
     calcTotal();
 }
 
-function cambioMoneda(moneda,precio) {
-    if(moneda === "UYU"){
+
+function cambioMoneda(moneda, precio) {
+    if (moneda === "UYU") {
         return precio / 40;
-    }else{
+    } else {
         return precio;
     }
 }
@@ -32,20 +33,23 @@ function cambioMoneda(moneda,precio) {
 function showProductsArray(array) {
     let contenido = "";
 
+
     for (let i = 0; i < array.length; i++) {
 
         let producto = array[i];
 
-        let precioMoneda = cambioMoneda(producto.currency,producto.unitCost);
+        let precioMoneda = cambioMoneda(producto.currency, producto.unitCost);
         let sub = producto.count * precioMoneda;
 
         contenido += `
             <tr>
-                <td><img src='${producto.src}' width="50px" ></td>
+                <td><img src='${producto.src}' width="90px" ></td>
 
-                <td>${producto.name}</td>
+                <td>${producto.name}</td>   
 
-                <td><input style="width:60px;" onchange="calcSubtotal(${producto.unitCost},${i})" 
+                <td> ${producto.currency} ${producto.unitCost}</td>
+
+                <td><input style="width:60px;" onchange="calcSubtotal(${precioMoneda},${i})" 
                 type="number" id="cantidad${i}" value="${producto.count}" min="1"></td>
 
                 <th>USD</th>
@@ -70,7 +74,7 @@ function calcEnvio() {
             envio = parseInt(elements[i].value);
         }
     }
-    let costoEnvio = (total * envio)/100 ;
+    let costoEnvio = (total * envio) / 100;
     let totalConEnvio = total + costoEnvio;
 
     let contenido = `
@@ -80,9 +84,56 @@ function calcEnvio() {
                     <td>${totalConEnvio}</td>
                     </tr>
                     `
-                    document.getElementById("totalTotal").innerHTML= contenido;
+    document.getElementById("totalTotal").innerHTML = contenido;
 
 }
+
+function seleccionarPago() {
+
+    var tipoPago = document.getElementsByName("formaPago");
+
+    for (var i = 0; i < tipoPago.length; i++) {
+        if (tipoPago[i].checked && tipoPago[i].value == "1") {
+            document.getElementById("datosTarjeta").classList.remove("d-none");
+            document.getElementById("datosBanco").classList.add("d-none");
+        } else if (tipoPago[i].checked && tipoPago[i].value == "2") {
+            document.getElementById("datosTarjeta").classList.add("d-none");
+            document.getElementById("datosBanco").classList.remove("d-none");
+        }
+    }
+}
+
+function pagoValidado() {
+    let formaPago = document.getElementsByName("formaPago");
+    let numTarjeta = document.getElementById("numTarjeta").value;
+    let titularTarjeta = document.getElementById("titularTarjeta").value;
+    let codigoTarjeta = document.getElementById("codigoTarjeta").value;
+    let cuenta = document.getElementById("cuenta").value;
+    let sucursal = document.getElementById("sucursal").value;
+    let titularCuenta = document.getElementById("titularCuenta").value;
+    let pagoValido = true;
+
+    for (var i = 0; i < formaPago.length; i++) {
+        if (formaPago[i].checked && (formaPago[i].value) == "1") {
+            if (numTarjeta == "" || titularTarjeta == "" || codigoTarjeta == "") {
+                pagoValido = false;
+            } else {
+                pagoValido = true;
+            }
+
+        } else if (formaPago[i].checked && (formaPago[i].value) == "2") {
+            if (cuenta == "" || sucursal == "" || titularCuenta == "") {
+                pagoValido = false;
+            } else {
+                pagoValido = true;
+            }
+        }
+
+    }
+    return pagoValido;
+}
+
+
 
 document.addEventListener("DOMContentLoaded", function (e) {
 
@@ -91,16 +142,108 @@ document.addEventListener("DOMContentLoaded", function (e) {
             productsArray = resultObj.data.articles;
 
             showProductsArray(productsArray);
-        
+
             calcEnvio();
         }
     });
 
     let elements = document.getElementsByName("envio");
-    for (var i=0; i<elements.length; i++){
-        elements[i].addEventListener("change",function () {
+    for (var i = 0; i < elements.length; i++) {
+        elements[i].addEventListener("change", function () {
             calcEnvio();
         });
     }
+
+    let tipoPago = document.getElementsByName("formaPago");
+    for (var i = 0; i < tipoPago.length; i++) {
+        tipoPago[i].addEventListener("change", function () {
+            seleccionarPago();
+        });
+    }
+
+
+
+    let form = document.getElementById('needs-validation');
+  
+
+
+    form.addEventListener('submit', function(e) {
+
+        if (form.checkvalidity() === false) {
+            e.preventDefault();
+            e.stopPropagation();
+
+            if (pagoValidado()) {
+                document.getElementById("btn-modal").classList.remove("btn-primary");
+                document.getElementById("btn-modal").classList.remove("btn-danger");
+                document.getElementById("btn-modal").classList.add("btn-success");
+
+                document.getElementById("datosValidados").innerHTML = `
+                <div class="alert alert alert-success" role="alert">
+                <strong>Forma de pago ingresada!</strong>
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+                `;
+
+            } else {
+                e.preventDefault();
+                e.stopPropagation();
+
+                document.getElementById("btn-modal").classList.remove("btn-primary");
+                document.getElementById("btn-modal").classList.add("btn-danger");
+                document.getElementById("btn-modal").classList.remove("btn-success");
+
+                document.getElementById("datosValidados").innerHTML = `
+
+                <div class="alert alert alert-danger" role="alert">
+                <strong>Debe ingresar una forma de pago!</strong>
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+                `;
+            }
+
+
+        } else {
+
+            if (pagoValidado()) {
+                document.getElementById("contenidoCarrito").innerHTML = `
+            <div class="alert alert alert-success" role="alert">
+            <strong>Felicitaciones!</strong> La compra se ha realizado con exito.
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div> 
+            `;
+
+            } else {
+                e.preventDefault();
+                e.stopPropagation();
+
+                document.getElementById("btn-modal").classList.remove("btn-primary");
+                document.getElementById("btn-modal").classList.add("btn-danger");
+                document.getElementById("btn-modal").classList.remove("btn-success");
+
+                document.getElementById("datosValidados").innerHTML = `
+                <div class="alert alert alert-danger" role="alert">
+                <strong>Debe ingresar una forma de pago!</strong>
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+                `;
+
+            }
+
+
+        }
+
+        form.classList.add('was-validated');
+
+    });
+
 
 });
